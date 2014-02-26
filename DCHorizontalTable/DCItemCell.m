470da84c5c7d380866a4e9c36a7ad5af1b2979a1
Yi Wang <yiwang1982@Yis-MacBook-Air.local>
2014-02-26 01:42:35 -0500

    changed colors

--- DCHorizontalTable/DCItemCell.m
+++ DCHorizontalTable/DCItemCell.m
@@ -27,9 +27,8 @@
         
         // Allocate and initialize a new UIImageView and give it a custom frame. As you will see we don’t position it at 0, 0 (the top left corner) but we give it a little bit of padding, as you remember a while back I told you this will allow for separation between each article cell.
         self.thumbnail = [[UIImageView alloc] initWithFrame:CGRectMake(kArticleCellHorizontalInnerPadding, kArticleCellVerticalInnerPadding, kCellWidth - kArticleCellHorizontalInnerPadding * 2, kCellHeight - kArticleCellVerticalInnerPadding * 2)];
-        
-        //self.thumbnail.layer.cornerRadius = self.thumbnail.frame.size.width * 0.5;
-        
+        // TODO: why isnt this working??
+        self.thumbnail.layer.cornerRadius = self.thumbnail.frame.size.width * 0.5f;
         
         // Set the thumbnail as opaque, Apple recommends that all views are opaque when possible as it will greatly improve the performance of our UI.
         self.thumbnail.opaque = YES;
@@ -37,19 +36,20 @@
         // Add the thumbnail as a subview of the cell’s content view, there’s a difference between adding it as a subview of the cell, and a subview of the cell’s content view!
         [self.contentView addSubview:self.thumbnail];
         
+
         // Create a UILabel and for the frame we make some adjustments so that it has some padding on the left side (thus preventing text to start from the very edge of the cell) and we make the height of the title label just 37% of the total height of the picture and we move it down a bit so it fits the bottom part of the cell.
         self.titleLabel = [[UILabel alloc] initWithFrame:CGRectMake(0, self.thumbnail.frame.size.height * 0.632, self.thumbnail.frame.size.width, self.thumbnail.frame.size.height * 0.37)];
         
         // This will make the title not cover the entire article cell and it’s image, but just the bottom half part. We then set the label as opaque, change the background color to a semitransparent gray, give it a white color for the text, set its font to the default bold font with a size of 11 points, allow the label to show in two lines and finally add it as a subview of the thumbnail.
         self.titleLabel.opaque = YES;
-        self.titleLabel.backgroundColor = [UIColor colorWithRed:0 green:0.4745098 blue:0.29019808 alpha:0.9];
+        self.titleLabel.backgroundColor = [UIColor colorWithRed:0.694 green:0.388 blue:0.639 alpha:0.9];
         self.titleLabel.textColor = [UIColor whiteColor];
         self.titleLabel.font = [UIFont boldSystemFontOfSize:11];
         self.titleLabel.numberOfLines = 2;
         [self.thumbnail addSubview:self.titleLabel];
         
         // Set the background color of the cell to something along the lines of our interface, we change the size of the cell’s background view to the same size as our thumbnail and give it the custom background color we added in our constants file earlier.
-        self.backgroundColor = [UIColor colorWithRed:0 green:0.40784314 blue:0.21568627 alpha:1.0];
+        self.backgroundColor = [UIColor whiteColor];
         self.selectedBackgroundView = [[UIView alloc] initWithFrame:self.thumbnail.frame];
         self.selectedBackgroundView.backgroundColor = kHorizontalTableSelectedBackgroundColor;
         
